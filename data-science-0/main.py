662d31ac4e62142aa12f782f19190c309f81ae11
matheusflausino <matheusflausino@ymail.com>
2020-04-22 11:49:51 -0300

    💪Desafio 0 - NOTA 100%

---
+++ data-science-0/main.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Desafio 1
+# 
+# Para esse desafio, vamos trabalhar com o data set [Black Friday](https://www.kaggle.com/mehdidag/black-friday), que reúne dados sobre transações de compras em uma loja de varejo.
+# 
+# Vamos utilizá-lo para praticar a exploração de data sets utilizando pandas. Você pode fazer toda análise neste mesmo notebook, mas as resposta devem estar nos locais indicados.
+# 
+# > Obs.: Por favor, não modifique o nome das funções de resposta.
+
+# ## _Set up_ da análise
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+black_friday = pd.read_csv("black_friday.csv")
+
+
+# ## Inicie sua análise a partir daqui
+
+# In[56]:
+
+
+# black_friday.head(10)
+# max(black_friday.isna().sum()) / len(black_friday.index)
+
+
+# black_friday_norm = (black_friday - black_friday.mean()) / (black_friday.max() - black_friday.min())
+# black_friday.max() - black_friday.min()
+
+
+
+black_friday['Product_Category_3'].dropna().mode()
+
+
+# ## Questão 1
+# 
+# Quantas observações e quantas colunas há no dataset? Responda no formato de uma tuple `(n_observacoes, n_colunas)`.
+
+# In[4]:
+
+
+def q1():
+    # Retorne aqui o resultado da questão 1.
+    return black_friday.shape
+
+
+# ## Questão 2
+# 
+# Há quantas mulheres com idade entre 26 e 35 anos no dataset? Responda como um único escalar.
+
+# In[17]:
+
+
+def q2():
+    # Retorne aqui o resultado da questão 2.
+    df_filterfame = black_friday['Gender'] == 'F'
+    df_filterage = black_friday['Age'] == '26-35'
+    return len(black_friday[df_filterfame & df_filterage].index)
+
+
+# ## Questão 3
+# 
+# Quantos usuários únicos há no dataset? Responda como um único escalar.
+
+# In[6]:
+
+
+def q3():
+    # Retorne aqui o resultado da questão 3.
+    return black_friday['User_ID'].nunique()
+
+
+# ## Questão 4
+# 
+# Quantos tipos de dados diferentes existem no dataset? Responda como um único escalar.
+
+# In[7]:
+
+
+def q4():
+    # Retorne aqui o resultado da questão 4.
+    return black_friday.dtypes.nunique()
+
+
+# ## Questão 5
+# 
+# Qual porcentagem dos registros possui ao menos um valor null (`None`, `ǸaN` etc)? Responda como um único escalar entre 0 e 1.
+
+# In[8]:
+
+
+def q5():
+    # Retorne aqui o resultado da questão 5.
+    return float(max(black_friday.isna().sum()) / len(black_friday.index))
+
+
+# ## Questão 6
+# 
+# Quantos valores null existem na variável (coluna) com o maior número de null? Responda como um único escalar.
+
+# In[9]:
+
+
+def q6():
+    # Retorne aqui o resultado da questão 6.
+    return int(max(black_friday.isna().sum()))
+
+
+# ## Questão 7
+# 
+# Qual o valor mais frequente (sem contar nulls) em `Product_Category_3`? Responda como um único escalar.
+
+# In[10]:
+
+
+def q7():
+    # Retorne aqui o resultado da questão 7.
+    return int(black_friday['Product_Category_3'].dropna().mode())
+
+
+# ## Questão 8
+# 
+# Qual a nova média da variável (coluna) `Purchase` após sua normalização? Responda como um único escalar.
+
+# In[11]:
+
+
+def q8():
+    # Retorne aqui o resultado da questão 8.
+    df = black_friday['Purchase']
+    normalized_df=(df-df.min())/(df.max()-df.min())
+    return float(normalized_df.mean())
+
+
+# ## Questão 9
+# 
+# Quantas ocorrências entre -1 e 1 inclusive existem da variáel `Purchase` após sua padronização? Responda como um único escalar.
+
+# In[12]:
+
+
+def q9():
+    # Retorne aqui o resultado da questão 9.
+    df_purchase = black_friday['Purchase']
+    df_purchase_zscore = (df_purchase - df_purchase.mean())/df_purchase.std(ddof=0)
+    return int(len(df_purchase_zscore[(df_purchase_zscore > -1) & (df_purchase_zscore < 1)]))
+
+
+# ## Questão 10
+# 
+# Podemos afirmar que se uma observação é null em `Product_Category_2` ela também o é em `Product_Category_3`? Responda com um bool (`True`, `False`).
+
+# In[13]:
+
+
+def q10():
+    # Retorne aqui o resultado da questão 10.
+    df = black_friday[['Product_Category_2', 'Product_Category_3']]
+    df = df[df['Product_Category_2'].isna()]
+    return df['Product_Category_2'].equals(df['Product_Category_3'])
+
